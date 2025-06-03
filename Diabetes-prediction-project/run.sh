1e176c5d12c681f2e702cc27110343c4c7f38c49
Praveenkumar101508 <praveenkamineti@gmail.com>
2025-06-03 22:32:33 +0100

    Add files via upload

---
+++ Diabetes-prediction-project/run.sh
@@ -0,0 +1,16 @@
+# Navigate to your backend directory
+cd backend
+
+# Create the virtual environment
+python3 -m venv venv
+
+# Activate the virtual environment
+source venv/bin/activate
+
+# Your terminal prompt should change, indicating the virtual environment is active
+
+# Install requirements (if you have a requirements.txt file)
+pip install -r requirements.txt
+
+# When you're done, you can deactivate the virtual environment
+deactivate
